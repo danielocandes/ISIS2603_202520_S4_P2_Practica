504c12a0fb4f904315b8fd9c1de6a385cb5a7621
ocamper <danielocampolozano@gmail.com>
2025-11-19 08:23:42 -0500

    emprendedor service hecho pero no lo he probado equisde

---
+++ src/app/emprendedor/emprendedor.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmprendedorDetail } from './emprendedor-detail';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmprendedorService {
+    private url = 'https://raw.githubusercontent.com/k-garces/ISIS2603_202520_S4_P2_Practica/refs/heads/main/json/emprendedores.json';
+
+    constructor(private http: HttpClient) { }
+
+    getEmprendedores(): Observable<EmprendedorDetail[]> {
+        return this.http.get<EmprendedorDetail[]>(this.url);
+    }
+
+    getEmprendedorDetail(id: number): Observable<EmprendedorDetail> {
+        const detailUrl = `${this.url}/${id}/emprendedor.json`;
+        return this.http.get<EmprendedorDetail>(detailUrl);
+    }
+}
